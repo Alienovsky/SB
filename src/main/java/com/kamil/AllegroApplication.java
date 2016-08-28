923a2c19b1b7c5601979d30d92bea555970de932
Alien <kontaktzbogiem@gmail.com>
2016-08-29 01:27:50 +0200

    first commit

---
+++ src/main/java/com/kamil/AllegroApplication.java
@@ -0,0 +1,12 @@
+package com.kamil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroApplication {
+		//https://springframework.guru/spring-boot-web-application-part-2-using-thymeleaf/
+	public static void main(String[] args) {
+		SpringApplication.run(AllegroApplication.class, args);
+	}
+}
